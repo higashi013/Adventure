f705917cf6b97837aae848f039cd69ae8439146c
Higashi Atsuto <att.hgs@gmail.com>
2020-08-19 19:27:33 +0900

    init
    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    int character;
+    float s;
+    int jump, jump_max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        character = character_select.getCN();
+        Debug.Log(character);
+        if(character == 2){
+            s = 2f;
+            jump_max = 1;
+        }else if(character == 3){
+            s = 1f;
+            jump_max = 2;
+        }else{
+            s = 1f;
+            jump_max = 1;
+        }
+        jump = jump_max;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f,0f,0.1f*s);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f*s,0f,0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f,0f,-0.1f*s);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f*s,0f,0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jump > 0)
+        {
+            rb.AddForce(0f,300f,0f);
+            jump -= 1;
+        }
+
+        if(transform.position.y < -5f){
+            transform.position = new Vector3(0f,0f,0f);
+        }
+    }
+    void OnCollisionEnter(Collision col){
+        if(col.gameObject.tag == "ground"){
+            jump = jump_max;
+        }
+    }
+}
---
+++ Assets/Scripts/character_select.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class character_select : MonoBehaviour
+{
+    public static int character_num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectNormal(){
+        character_num = 1;
+        SceneManager.LoadScene("game");
+    }
+    public void SelectOctone(){
+        character_num = 2;
+        SceneManager.LoadScene("game");
+    }
+    public void SelectPathfinder(){
+        character_num = 3;
+        SceneManager.LoadScene("game");
+    }
+    public static int getCN(){
+        return character_num;
+    }
+}
